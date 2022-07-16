cd1c8f6c3f0d29f21372b09a534c959866346c06
SasinduNanayakkara <it20625016@my.sliit.lk>
2022-07-16 16:06:26 +0530

    Add balance features

--- src/token_assets/src/components/Balance.jsx
+++ src/token_assets/src/components/Balance.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import {Principal} from "@dfinity/principal";
+import {token} from "../../../declarations/token"
 
 function Balance() {
   
+  const [inputValue, setInputValue] = useState("");
+  const [balanceResult, setBalanceResult] = useState("");
+  const [cryptoSymbol, setCryptoSymbol] = useState("");
+  const [isHidden, setIsHidden] = useState(true);
+
   async function handleClick() {
-    console.log("Balance Button Clicked");
+    // console.log(inputValue);
+    const principal = Principal.fromText(inputValue);
+    const balance = await token.balanceOf(principal);
+    setBalanceResult(balance.toLocaleString());
+    setCryptoSymbol(await token.getSymbol());
+    setIsHidden(false);
   }
 
 
@@ -15,6 +27,8 @@ function Balance() {
           id="balance-principal-id"
           type="text"
           placeholder="Enter a Principal ID"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
         />
       </p>
       <p className="trade-buttons">
@@ -25,7 +39,7 @@ function Balance() {
           Check Balance
         </button>
       </p>
-      <p>This account has a balance of XYZ.</p>
+      <p hidden={isHidden}>This account has a balance of {balanceResult} {cryptoSymbol}.</p>
     </div>
   );
 }
