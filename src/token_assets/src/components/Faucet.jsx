8f9620ece1ea1aea02046004dc3e64f773fe770d
SasinduNanayakkara <it20625016@my.sliit.lk>
2022-07-16 16:06:55 +0530

    Implement display balance feature

--- src/token_assets/src/components/Faucet.jsx
+++ src/token_assets/src/components/Faucet.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import {token} from "../../../declarations/token";
 
 function Faucet() {
 
-  async function handleClick(event) {
+  const [isDisable, setIsDesable] = useState(false);
+  const [buttonText, setButtonText] = useState("Gimme Gimme");
 
+  async function handleClick(event) {
+    setIsDesable(true);
+    const result = await token.payOut();
+    setButtonText(result);
+    // setIsDesable(false);
   }
 
   return (
@@ -16,8 +23,8 @@ function Faucet() {
       </h2>
       <label>Get your free DAngela tokens here! Claim 10,000 DANG coins to your account.</label>
       <p className="trade-buttons">
-        <button id="btn-payout" onClick={handleClick}>
-          Gimme gimme
+        <button id="btn-payout" disabled={isDisable} onClick={handleClick}>
+          {buttonText}
         </button>
       </p>
     </div>
