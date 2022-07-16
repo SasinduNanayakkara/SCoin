b735c9812cdb77735496244ed6d911c0d0735e45
SasinduNanayakkara <it20625016@my.sliit.lk>
2022-07-16 16:07:23 +0530

    background configs

--- src/declarations/token/token.did.d.ts
+++ src/declarations/token/token.did.d.ts
@@ -1,3 +1,8 @@
 import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 
+export interface _SERVICE {
+  'balanceOf' : ActorMethod<[Principal], bigint>,
+  'getSymbol' : ActorMethod<[], string>,
+  'payOut' : ActorMethod<[], string>,
+}
