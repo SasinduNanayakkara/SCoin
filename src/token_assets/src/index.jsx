fe9d824517037fb8de3a383d2719e5a574250c57
SasinduNanayakkara <it20625016@my.sliit.lk>
2022-07-17 13:48:27 +0530

    Implement dfinity authentication features

--- src/token_assets/src/index.jsx
+++ src/token_assets/src/index.jsx
@@ -1,9 +1,28 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import App from "./components/App";
+import { AuthClient } from "@dfinity/auth-client";
 
 const init = async () => { 
-  ReactDOM.render(<App />, document.getElementById("root"));
+  
+  const Auth = await AuthClient.create();
+
+  if (await Auth.isAuthenticated()) {
+    handleAuthentication(Auth);
+  }
+  else {
+    await Auth.login({
+      identityProvider: "https://identity.ic0.app/#authorize",
+      onSuccess: () => {
+      }
+    });
+  }
+}
+
+async function handleAuthentication(Auth) {
+  const identity = await Auth.getIdentity();
+  const userPricipal = identity._principal.toString();
+  ReactDOM.render(<App loggedInPrincipal={userPricipal}/>, document.getElementById("root"));
 }
 
 init();
