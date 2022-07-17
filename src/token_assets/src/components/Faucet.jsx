8aaba7a16f24aa7014ca6acfc9fb2029273e9563
SasinduNanayakkara <it20625016@my.sliit.lk>
2022-07-17 13:52:24 +0530

    Implement user authentication feature

--- src/token_assets/src/components/Faucet.jsx
+++ src/token_assets/src/components/Faucet.jsx
@@ -1,14 +1,25 @@
 import React, { useState } from "react";
-import {token} from "../../../declarations/token";
+import {token, canisterId, createActor} from "../../../declarations/token";
+import { AuthClient } from "@dfinity/auth-client";
 
-function Faucet() {
+function Faucet(props) {
 
   const [isDisable, setIsDesable] = useState(false);
   const [buttonText, setButtonText] = useState("Gimme Gimme");
 
   async function handleClick(event) {
     setIsDesable(true);
-    const result = await token.payOut();
+
+    const authClient = await AuthClient.create();
+    const identity = await authClient.getIdentity();
+
+    const authenticatedCanister = createActor(canisterId, {
+      agentOptions: {
+        identity,
+      },
+    });
+
+    const result = await  authenticatedCanister.payOut();
     setButtonText(result);
     // setIsDesable(false);
   }
@@ -21,7 +32,7 @@ function Faucet() {
         </span>
         Faucet
       </h2>
-      <label>Get your free DAngela tokens here! Claim 100 DANG coins to your account.</label>
+      <label>Get your free DAngela tokens here! Claim 100 DANG coins to {props.userPrincipal}.</label>
       <p className="trade-buttons">
         <button id="btn-payout" disabled={isDisable} onClick={handleClick}>
           {buttonText}
